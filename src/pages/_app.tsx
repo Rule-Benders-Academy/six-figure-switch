466c3dbc4c0a5ecf711a67a97fed23e24ea4266d
jeremy delacruz <jeremy.delacruz.dev@gmail.com>
2025-08-25 07:56:38 +0800

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -36,6 +36,25 @@ export default function App({ Component, pageProps }: AppProps) {
         }}
       />
 
+      {/* Crisp Chat */}
+      <Script
+        id="crisp-chat"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.$crisp = [];
+            window.CRISP_WEBSITE_ID = "92580adf-bda9-44c7-a1ce-a83b917cea7b";
+            (function () {
+              var d = document;
+              var s = d.createElement("script");
+              s.src = "https://client.crisp.chat/l.js";
+              s.async = 1;
+              d.getElementsByTagName("head")[0].appendChild(s);
+            })();
+          `,
+        }}
+      />
+
       {/* Your app content */}
       <Component {...pageProps} />
     </>
