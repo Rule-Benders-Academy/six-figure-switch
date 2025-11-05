572a04794b763b889bf5004d69dd3df1d335e5a0
jeremy delacruz <jeremy.delacruz.dev@gmail.com>
2025-11-05 13:23:36 +0800

    add redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/masterclass-video",
+        destination: "/dynamic-masterclass-video",
+        permanent: false, // set true if this is a permanent move
+      },
+    ];
+  },
 };
 
 export default nextConfig;
